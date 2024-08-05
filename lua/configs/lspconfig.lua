207230e867b930c5b1f108f0871395856106bcee
Côme Vincent <come.vincent@tiempo-secure.com>
2024-08-05 11:32:08 +0200

    upadte locks and clean lsp config

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,18 +1,6 @@
-local on_attach = require("nvchad.configs.lspconfig").on_attach
-local on_init = require("nvchad.configs.lspconfig").on_init
-local capabilities = require("nvchad.configs.lspconfig").capabilities
-
 local lspconfig = require "lspconfig"
 
-lspconfig.clangd.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
-  cmd = {
-    "clangd",
-    "--offset-encoding=utf-16",
-  },
-}
+lspconfig.clangd.setup {}
 
 lspconfig.pylsp.setup {}
 
